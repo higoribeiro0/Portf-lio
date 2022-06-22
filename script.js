f3b8434c080f2d217b055c53b1d2fd14d1d47035
higoribeiro0 <higoribeiro0@gmail.com>
2022-06-21 22:56:33 -0300

    portfólio

---
+++ script.js
@@ -0,0 +1,12 @@
+let MenuArea2 = document.querySelector("nav")
+let clickm2 = document.querySelector('.menu-opened')
+
+clickm2.addEventListener('touchstart', function(){
+    if(MenuArea2.style.width == '35%'){
+        MenuArea2.style.width ='0';
+    } else {
+        MenuArea2.style.width ='35%';
+    }
+})
+
+
